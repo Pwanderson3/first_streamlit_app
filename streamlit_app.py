699425133fbcbc9569f9294543535e067ddd89ef
Pwanderson3 <143218135+Pwanderson3@users.noreply.github.com>
2023-08-25 15:18:49 -0400

    Update streamlit_app.py
    Only display rows in dataset selected in the multiselect

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,11 +12,11 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-# Choose the Fruit Name Column as the Index
-my_fruit_list = my_fruit_list.set_index('Fruit')
+my_fruit_list = my_fruit_list.set_index('Fruit') # Choose the Fruit Name Column as the Index
 
 # Pick list so user can pick the fruit they want to include
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table
-streamlit.dataframe(my_fruit_list)
+streamlit.dataframe(fruits_to_show)
