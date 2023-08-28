ea6566014169019c3331694336064a1a35cb5f41
Pwanderson3 <143218135+Pwanderson3@users.noreply.github.com>
2023-08-28 16:55:49 -0400

    Update streamlit_app.py
    Added function to insert text to table

--- streamlit_app.py
+++ streamlit_app.py
@@ -72,7 +72,18 @@ streamlit.stop()
 
 # Add second text entry box
 # Allow the end user to add a fruit to the list
-add_my_fruit = streamlit.text_input('What frfuit would you like to add?','jackfruit')
-streamlit.write('Thanks for adding ', add_my_fruit)
+der insert_row_snowflake(new_fruit):
+    with my_cnx.cursor() as my_cur:
+        my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+        return "Thanks for adding " + new_fruit
 
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+
+add_my_fruit = streamlit.text_input('What frfuit would you like to add?')
+if streamlit.button('Add a Fruit to the List'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    back_from_function = insert_row_snowflake(add_my_fruit)
+    streamlit.text(back_from_function)
+
+# streamlit.write('Thanks for adding ', add_my_fruit)
+
+# my_cur.execute("insert into fruit_load_list values ('from streamlit')")
